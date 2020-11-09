b1c17b1e639e558362dbfe1cc8aee84d10cfed86
rutger heyvaert <rutger.heyvaert@student.howest.be>
2020-11-09 18:10:55 +0100

    added get colonies empty

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -8,14 +8,6 @@ public class MarsController {
     }
 
     public Object getColonies() {
-        JsonObject res1 = new JsonObject();
-        res1.put("uuid", 1);
-        res1.put("name", "Colony1");
-        res1.put("location", "location1");
-        JsonObject res2 = new JsonObject();
-        res2.put("uuid", 1);
-        res2.put("name", "Colony1");
-        res2.put("location", "location1");
-        return "["+res1+","+res2+"]";
+        return null;
     }
 }
