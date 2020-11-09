f0e87046eadf12a7019158be15ce3108b2868276
rutger heyvaert <rutger.heyvaert@student.howest.be>
2020-11-09 18:09:59 +0100

    added 2 mock colonies for front-end

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,7 +1,21 @@
 package be.howest.ti.mars.logic.controller;
 
+import io.vertx.core.json.JsonObject;
+
 public class MarsController {
     public String getMessage() {
         return "Hello, Mars!";
     }
+    public String getColonies() {
+        JsonObject res1 = new JsonObject();
+        res1.put("uuid", 1);
+        res1.put("name", "Colony1");
+        res1.put("location", "location1");
+        JsonObject res2 = new JsonObject();
+        res2.put("uuid", 1);
+        res2.put("name", "Colony1");
+        res2.put("location", "location1");
+        return "["+res1+","+res2+"]";
+    }
+
 }
