716352842e9ed3cf8feeee75d4cd3ccfd3cc2797
rutger heyvaert <rutger.heyvaert@student.howest.be>
2020-11-09 20:20:34 +0100

    fixed now array list of objects type JsonObject

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -8,6 +8,17 @@ public class MarsController {
     }
 
     public Object getColonies() {
-        return null;
+        JsonObject res1 = new JsonObject();
+        res1.put("id", 1);
+        res1.put("name", "Colony1");
+        res1.put("location", "location1");
+        JsonObject res2 = new JsonObject();
+        res2.put("id", 2);
+        res2.put("name", "Colony2");
+        res2.put("location", "location2");
+        JsonObject[] res = new JsonObject[2];
+        res[0]= res1;
+        res[1]= res2;
+        return res;
     }
 }
