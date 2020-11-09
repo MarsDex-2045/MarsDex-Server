5def7a7d1934b7f241f5d4374699a884616afaea
rutger heyvaert <rutger.heyvaert@student.howest.be>
2020-11-09 17:45:59 +0100

    added the method getColonies still empty

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,7 +1,13 @@
 package be.howest.ti.mars.logic.controller;
 
+import io.vertx.core.json.JsonObject;
+
 public class MarsController {
     public String getMessage() {
         return "Hello, Mars!";
+    }
+
+    public Object getColonies() {
+        return null;
     }
 }
