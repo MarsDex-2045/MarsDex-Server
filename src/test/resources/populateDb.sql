07a0fc3ea2b5ff9b91c06b0135f629b7fc9088c8
Bo <bo.robbrecht@student.howest.be>
2020-11-11 12:48:30 +0100

    Added values for independent tables

---
+++ src/test/resources/populateDb.sql
@@ -0,0 +1,17 @@
+insert into companies(name, email, phone, storage)
+VALUES ('MarsDex', 'marsdex@mars.com', '+6623145878', null);
+VALUES ('MaMiCo', 'mamico@mars.com', '+3422893567', '150000');
+VALUES ('Generic Company', 'generic@earth.com', '+3246777245', '1000000');
+
+insert into colonies(name, latitude, longitude, altitude)
+VALUES ('Haberlandt Survey', 0.00000, 0.00000, 0.000 );
+VALUES ('Durrance Camp', 40.22451, -80.56218, 160.000);
+VALUES ('Ehrlich City', 33.21322, -33.2132, 300.000);
+VALUES ('Silves Claim', 22.21773, 24.33564, -200.232);
+
+insert into resources(price, name)
+VALUES (71.596, 'Painite');
+VALUES (271.192, 'Alexandrite');
+VALUES (1.077, 'Bauxite');
+VALUES (232.553, 'Low Temperature Diamonds');
+VALUES (135,218, 'Void Opals');
