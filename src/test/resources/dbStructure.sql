ebf100f4cd79f8fde93491ed48b80ff945734019
Bo <bo.robbrecht@student.howest.be>
2020-11-11 12:42:48 +0100

    Added forgotten column in DB

--- src/test/resources/dbStructure.sql
+++ src/test/resources/dbStructure.sql
@@ -28,7 +28,8 @@ CREATE TABLE IF NOT EXISTS companies(
 
 CREATE TABLE IF NOT EXISTS resources (
     id int primary key auto_increment not null,
-    price double not null
+    price double not null,
+    name nvarchar(30) not null
 );
 
 CREATE TABLE IF NOT EXISTS colonies_companies(
