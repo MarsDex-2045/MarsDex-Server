0a1675a8f559739d056d45444101baccf80877d6
Bo <bo.robbrecht@student.howest.be>
2020-11-12 13:13:53 +0100

    Made Notification class

---
+++ src/main/java/be/howest/ti/mars/logic/classes/Notification.java
@@ -0,0 +1,37 @@
+package be.howest.ti.mars.logic.classes;
+
+import io.vertx.core.json.JsonObject;
+
+import java.util.Objects;
+
+public class Notification {
+    int id;
+    String heading;
+    String message;
+
+    public Notification(int id, String heading, String message) {
+        this.id = id;
+        this.heading = heading;
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notification that = (Notification) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public JsonObject toJSON(){
+        JsonObject json = new JsonObject();
+        json.put("heading", this.heading);
+        json.put("message", this.message);
+        return json;
+    }
+}
