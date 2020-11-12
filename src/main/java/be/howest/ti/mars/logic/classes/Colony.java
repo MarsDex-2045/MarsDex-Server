623159ff40546e56602dc2e24eb24daf6e34e7a4
rutger heyvaert <rutger.heyvaert@student.howest.be>
2020-11-12 10:08:14 +0100

    added the list of resources

--- src/main/java/be/howest/ti/mars/logic/classes/Colony.java
+++ src/main/java/be/howest/ti/mars/logic/classes/Colony.java
@@ -1,19 +1,18 @@
 package be.howest.ti.mars.logic.classes;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Colony {
     private final int id;
     private final Location location;
     private final String name;
-
-    public Colony(int id,  String name,Location location) {
-        this.id = id;
-        this.location = location;
-        this.name = name;
-    }
+    HashMap<Integer, Resource> resources = new HashMap<>();
 
     public int getId() {
         return id;
@@ -27,11 +26,31 @@ public class Colony {
         return name;
     }
 
+    public HashMap<Integer, Resource> getResources() {
+        return resources;
+    }
+
+    public Colony(int id, String name, Location location, JsonObject resources) {
+        this.id = id;
+        this.location = location;
+        this.name = name;
+    }
+    public void AddResource(int id, String name, double price, double weight, Calendar addDate){
+    Resource add = new Resource(id,name,price,weight,addDate);
+        resources.put(id,add);
+    }
+    public ArrayList<Resource> GetAllResources(){
+        return new ArrayList<>(resources.values());
+    }
+
+
     public JsonObject toJson(){
         JsonObject ColonyJSON = new JsonObject();
         ColonyJSON .put("id", this.getId());
         ColonyJSON .put("name", this.getName());
         ColonyJSON .put("location", this.getLocation());
+
+
         return ColonyJSON ;
     }
 
