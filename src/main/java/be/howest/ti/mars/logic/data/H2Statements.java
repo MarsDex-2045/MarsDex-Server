b6f4808f3a1fbcbe9d02e582c3fc63717cc06dd6
Bo <bo.robbrecht@student.howest.be>
2020-11-19 17:46:27 +0100

    Made SQL Statement that filters & only displays what's needed for GET /colony/{id}

--- src/main/java/be/howest/ti/mars/logic/data/H2Statements.java
+++ src/main/java/be/howest/ti/mars/logic/data/H2Statements.java
@@ -2,7 +2,19 @@ package be.howest.ti.mars.logic.data;
 
 class H2Statements {
     protected static final String H2_GET_COLONIES = "SELECT * FROM MARSDEX.COLONIES";
-    protected static final String H2_GET_COLONY= "SELECT * FROM MARSDEX.COLONIES WHERE ID = ?";
+    protected static final String H2_GET_COLONY= constructColonySQLStatement();
+
+    private static String constructColonySQLStatement() {
+        StringBuilder res = new StringBuilder();
+        res.append("SELECT CN.NAME, CN.NAME, CN.LATITUDE, CN.LONGITUDE,  CN.ALTITUDE, R.NAME, CR.WEIGHT, CR.ADDED_TIMESTAMP, R.PRICE ")
+                .append("FROM MARSDEX.COLONIES CN ")
+                .append("JOIN MARSDEX.COLONIES_COMPANIES CC ON CN.ID = CC.COLONY_ID ")
+                .append("JOIN MARSDEX.COMPANIES CM ON CC.COMPANY_ID = CM.ID ")
+                .append("JOIN MARSDEX.COMPANIES_RESOURCES CR ON CM.ID = CR.COMPANY_ID ")
+                .append("JOIN MARSDEX.RESOURCES R ON CR.RESOURCE_ID = R.ID ")
+                .append("WHERE CN.ID = ?");
+        return res.toString();
+    }
 
     private H2Statements() {
     }
