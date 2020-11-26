ba8af2f79d47d2e9a69fd4d00b943ca1232cf57c
Bo <bo.robbrecht@student.howest.be>
2020-11-26 18:00:53 +0100

    Added MarsController tests

--- src/test/java/be/howest/ti/mars/logic/controller/MarsControllerTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/MarsControllerTest.java
@@ -1,11 +1,42 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.classes.Colony;
+import be.howest.ti.mars.logic.classes.Company;
+import be.howest.ti.mars.logic.classes.Location;
+import be.howest.ti.mars.logic.data.MarsRepository;
+import be.howest.ti.mars.logic.exceptions.IdentifierException;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import org.apache.commons.lang3.StringUtils;
+import org.h2.tools.RunScript;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MarsControllerTest {
+    public static final Logger LOGGER = Logger.getLogger(MarsController.class.getName());
+
+    @BeforeEach
+    void setupTest() throws IOException, SQLException {
+        try (Connection con = MarsRepository.getInstance().getConnection()){
+            executeScript("src/test/resources/dbClean.sql", con);
+            executeScript("src/test/resources/dbConstruction.sql", con);
+        }
+    }
+
+    private void executeScript(String filePath, Connection con) throws IOException, SQLException {
+        RunScript.execute(con, new FileReader(filePath));
+        LOGGER.log(Level.INFO, "Executed SQL File from " + filePath);
+    }
 
     @Test
     void getMessageReturnsAWelcomeMessage() {
@@ -17,5 +48,50 @@ class MarsControllerTest {
 
         //Assert
         assertTrue(StringUtils.isNoneBlank(message));
+    }
+
+    @Test
+    void getColonies() {
+        MarsRepository data = MarsRepository.getInstance();
+        Colony ref1 = new Colony(1,"Haberlandt Survey", new Location(0.00000, 0.00000, 0.000));
+        Colony ref2 = new Colony(2, "Durrance Camp", new Location(40.22451, -80.56218, 160.000));
+        Colony ref3 = new Colony(3, "Ehrlich City", new Location(33.21322, -33.2132, 300.000));
+        Colony ref4 = new Colony(4, "Silves Claim", new Location(22.21773, 24.33564, -200.232));
+
+        JsonArray json = new MarsController().getColonies();
+
+        assertTrue(json.contains(ref1.toShortJSON()));
+        assertTrue(json.contains(ref2.toShortJSON()));
+        assertTrue(json.contains(ref3.toShortJSON()));
+        assertTrue(json.contains(ref4.toShortJSON()));
+    }
+
+    @Test
+    void getColonyById() {
+        MarsRepository data = MarsRepository.getInstance();
+        Colony ref = new Colony(3, "Ehrlich City", new Location(33.21322, 	-33.2132, 300.0));
+        ref.addCompany(new Company(4, "Geminorum Blue Vison Partners", "V1s10na1r", "gbvp@mars.com", "+552434221", 150000));
+        ref.addCompany(new Company(5, "Hydrae Noblement Services", "8ydr0n", "hydraenoble@mars.com", "+454553234", 250000));
+
+        JsonObject json = data.getColony(3).toJSON();
+
+        assertEquals(3, json.getInteger("id"));
+        assertEquals("Ehrlich City", json.getString("name"));
+        assertTrue(json.containsKey("location"));
+        assertTrue(json.containsKey("resources"));
+        assertThrows(IdentifierException.class, () -> data.getColony(22));
+    }
+
+    @Test
+    void getCompanyResources() {
+        MarsRepository data = MarsRepository.getInstance();
+
+        JsonObject json = data.getCompany(2).allResourcesToJSONObject();
+        JsonArray resources = json.getJsonArray("resources");
+
+        assertEquals(2, json.getInteger("id"));
+        assertTrue(json.containsKey("resources"));
+        assertEquals(8, resources.size());
+        assertThrows(IdentifierException.class, () -> data.getCompany(22));
     }
 }
